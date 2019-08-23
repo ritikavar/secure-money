80e6de8423cb53b20bf88d1b749307c79eff0930
ritikavar <ritikavarshney5@gmail.com>
2019-08-24 01:25:59 +0530

    secure money

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.second;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    EditText email;
+    EditText password;
+    Button login;
+    String e,p;
+ @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                e=email.getText().toString();
+                p=password.getText().toString();
+
+                if (!e.isEmpty() && !p.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Successfully login", Toast.LENGTH_LONG).show();
+                } else if (e.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Email is empty", Toast.LENGTH_SHORT).show();
+                } else if (p.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Password is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "All fields are empty", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
